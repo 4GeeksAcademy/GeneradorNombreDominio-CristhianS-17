07495258c27cd1a8ee36193deea25180c6459143
CristhianS-17 <cristhianjss98@gmail.com>
2026-02-23 20:35:04 +0000

    GeneradorDominio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "my", "your", "his", "her", "it", "its", "our"];
+let adj = ["fantastic", "wonderful", "awesome", "amazing", "incredible", "spectacular", "fabulous"];
+let noun = ["dog", "driver", "phone", "robot", "book", "motorcycle", "city", "car", "house", "flower"];
+let dominio = [".com", ".net", ".org", ".es"];
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let a = 0; a < pronoun.length; a++) {
+  for (let h = 0; h < adj.length; h++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let c = 0; c < dominio.length; c++) {
+        console.log(pronoun[a] + adj[h] + noun[n] + dominio[c]);
+      }
+    }
+  }
+}
